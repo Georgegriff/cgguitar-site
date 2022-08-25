c6d6e1cc163836c94da19e98497e3f37823f8f83
George Griffiths <george.griffiths@immersivelabs.com>
2022-08-25 15:02:18 +0100

    removed javascript for plan select

--- src/scripts/book-widget.js
+++ src/scripts/book-widget.js
@@ -1,20 +1,22 @@
-const planWidget = document.querySelector("#tz-plan-widget-new");
-const config = { childList: true, subtree: true };
+// const planWidget = document.querySelector("#tz-plan-widget-new");
+// const config = { childList: true, subtree: true };
 
-let initialisedForm = false;
-const callback = () => {
-    const lessonTypeForm = planWidget.querySelector(".tz-plan-wg-instrument");
-    const lessonTypeOption = [...lessonTypeForm.options].find((option) => option.value !== "0");
-    if (lessonTypeOption) {
-        lessonTypeOption.selected = true;
-        initialisedForm = true;
-        observer.disconnect();
-    }
-};
+// let initialisedForm = false;
+// const callback = () => {
+//     const planForm = planWidget.querySelector(".tz-plan-wg-plan");
+//     debugger;
+//     const lessonTypeOption = [...planForm.options].find((option) => option.value !== "0");
+//     if (lessonTypeOption) {
+//         lessonTypeOption.selected = true;
+//         initialisedForm = true;
+//         observer.disconnect();
+//     }
+// };
   
-  // Create an observer instance linked to the callback function
-const observer = new MutationObserver(callback);
+//   // Create an observer instance linked to the callback function
+// const observer = new MutationObserver(callback);
   
-// Start observing the target node for configured mutations
-observer.observe(planWidget, config);
-
+// // Start observing the target node for configured mutations
+// observer.observe(planWidget, config);
+ 
+// doesnt currently work
