8620f4362e7c888a34dcad70e06fad8e38a4925d
George Griffiths <george.griffiths@immersivelabs.com>
2022-08-26 10:43:29 +0100

    added next/prev controls

--- src/scripts/index.js
+++ src/scripts/index.js
@@ -1,14 +1,2 @@
 import "./shared.js";
-
-document.querySelector('#controls').addEventListener('click', (event) => {
-    const $slide = document.querySelector(event.target.getAttribute('href'));
-    if (!$slide) return;
-    
-    if ($slide.scrollIntoViewIfNeeded) {
-      event.preventDefault();
-      $slide.scrollIntoViewIfNeeded();
-    } else if ($slide.scrollIntoView) {
-      event.preventDefault();
-      $slide.scrollIntoView();
-    }
-  });
+import './image-carousel.js'
